44160865fd0b465e9bddbf32f753c4dad6c2a3cf
rpsouto <rpsouto@lncc.br>
2021-04-06 10:43:07 -0300

    removendo o diretorio graficos do projeto

--- graficos/plotar.m
+++
@@ -1,9 +0,0 @@
-clear all
-close all
-load ../output/training/qModelExpA.out;
-load ../output/training/qObservExpA.out;
-load ../output/training/qAnalysisExpA.out;
-plot(qModelExpA)
-hold on
-plot(qObservExpA)
-plot(qAnalysisExpA,'g')
