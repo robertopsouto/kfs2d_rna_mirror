c522cb777e0ae938a243f93e29c55f16a5ed8a9f
rpsouto <rpsouto@lncc.br>
2021-04-06 21:35:13 -0300

    output/training/normalizar.m atualizado

--- output/training/normalizar.m
+++ output/training/normalizar.m
@@ -17,15 +17,19 @@ qM_norm=(max(qM)*valNormInf-min(qM)*valNormSup+qM*(valNormSup-valNormInf))/(max(
 qO_norm=(max(qO)*valNormInf-min(qO)*valNormSup+qO*(valNormSup-valNormInf))/(max(qO)-min(qO));
 qA_norm=(max(qA)*valNormInf-min(qA)*valNormSup+qA*(valNormSup-valNormInf))/(max(qA)-min(qA));
 
-n=size(qM);
+[l,n]=size(qM);
 pontos_por_ciclo=25;
-ciclos=n/pontos_por_ciclos;
+ciclos=n/pontos_por_ciclo;
 
-train_range=ceil(0.60*ciclos)*pontos_por_clicos;
+parc1 = ceil(0.60*ciclos)*pontos_por_ciclo;
+parc2 = ceil(0.10*ciclos)*pontos_por_ciclo;
+parc3 = n - parc1 - parc2;
+
+train_range=parc1;
 valid_init=train_range+1;
-valid_end=train_range+ceil(0.10*ciclos)*pontos_por_clicos;
+valid_end=train_range+parc2;
 gen_init=valid_end+1;
-gen_end=valid_end+ceil(0.30*ciclos)*pontos_por_clicos;;
+gen_end=valid_end+parc3;;
 
 fileID = fopen('x.txt','w');
 fprintf(fileID,'      %8.5f',qM_norm(1:train_range));
