a9e889730eac3595efea37f332795f09ead871fe
rpsouto <rpsouto@lncc.br>
2021-04-11 20:07:07 -0300

    Atualiza para incluir o ruido como um dos parametros de entrada

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Uso: \
 ```./run-KFS2d.sh assimType gridX gridY timeStep freqObsT freqObsX freqObsY neuronNumber ```
 
 Uso com Filtro de Kalman: \
-```./run-KFS2d.sh 1 10 10 60 10 2 2 10 ```
+```./run-KFS2d.sh 1 10 10 60 10 2 2 0.2 10 ```
 
 Uso com RNA: \
-```./run-KFS2d.sh 2 10 10 60 10 2 2 10 ```
+```./run-KFS2d.sh 2 10 10 60 10 2 2 0.2 10 ```
--- run-KFS2d.sh
+++ run-KFS2d.sh
@@ -27,26 +27,31 @@ if [[ ! -d $resultsdir ]]; then
   mkdir $resultsdir
 fi
 
-outputdir="output-gridX_$gridX-gridY_$gridY-timestep_$timeStep-freqObsT_$freqObsT-freqObsX_$freqObsX-freqObsY_$freqObsY-neuronNumber_$neuronNumber"
-if [[ ${assimType} -eq 1 && -d resultados/percNoise_${percNoise}/$resultdir ]]; then
+outputdir="output-gridX_$gridX-gridY_$gridY-timestep_$timeStep-freqObsT_$freqObsT-freqObsX_$freqObsX-freqObsY_$freqObsY"
+if [[ ${assimType} -eq 1 && -d ${resultsdir}/$outputdir ]]; then
   echo "Resultado com FK já gerado com estes parametros!"
   exit 1
 fi
+
+#Apaga os resultados anteriores
+rm output/*.out
+rm output/full/*.out
+rm output/training/*.out
 
 #Executa o KFS2d 
 ./KFS2d $assimType $gridX $gridY $timeStep $freqObsT $freqObsX $freqObsY $percNoise $neuronNumber 2>&1 | tee output.log 
 
 if [[ ${assimType} -eq 1 ]]; then
   echo "Copiando o resultado da assimilacao por FK e os dados para treinamento da RNA."
-  cp -r -p output/ ${resultdir}/$outputdir
-  mv output.log ${resultsdir}/${outputdir}/output_FK.log
+  cp -r -p output/ ${resultsdir}/$outputdir
+  cp output.log ${resultsdir}/${outputdir}/output_FK.log
 fi
 
-if [[ ${assimType} -eq 2 && -d resultados/$resultdir ]]; then
+if [[ ${assimType} -eq 2 && -d ${resultsdir}/$outputdir ]]; then
   echo "Copiando o resultado da assimilacao de FK emulada por RNA."
-  cp output/full/qAnalysisExpA_RNA.out ${resultsdir}/${outputdir}//full/
-  cp output/computingANNTime.out ${resultsdir}/${outputdir}/  
-  mv output.log ${resultsdir}/${outputdir}/output_RNA.log
+  cp output/full/qAnalysisExpA_RNA.out ${resultsdir}/${outputdir}/full/qAnalysisExpA_RNA-neuronNumber_${neuronNumber}.out
+  cp output/computingANNTime.out ${resultsdir}/${outputdir}/computingANNTime-neuronNumber_${neuronNumber}.out
+  cp output.log ${resultsdir}/${outputdir}/output_RNA-neuronNumber_${neuronNumber}.log
 fi
 
 
